5226c4e53e2b716e3d651bae56c7ddb51e320bb5
Ramon Meffert <ramon.meffert@gmail.com>
2021-09-16 21:28:23 +0200

    Add personal website

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# ramonmeffert.github.io
+# Personal website
 
-At this moment, this repository servers a single purpose:
-Because I renamed one of my repositories that had an associated GitHub Pages, the old URL broke.
-This repository implements [a trick](https://shoehornwithteeth.com/ramblings/2016/12/redirecting-github-pages-after-renaming-a-repository/) described by [Gilmore Davidson](https://shoehornwithteeth.com/) to fix this.
+This is the source code to my personal website, <r3n.nl>.
+
+## Setup
+
+This is a pretty simple static minisite (meaning, the output is only css + html). Tools I used:
+
+- Gulp (build pipeline)
+- Pug (cleaner html)
+- Sass (cleaner css)
+- BrowserSync (live-reload while developing)
+- Minifiers (html-minifier, cssnano)
+- Prefixers (autoprefixer)
---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+import gulp from 'gulp';
+import sass from 'gulp-dart-sass';
+import browserSync from 'browser-sync';
+import pug from 'gulp-pug';
+import htmlmin from 'gulp-htmlmin';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+import postcss from 'gulp-postcss';
+import del from 'del';
+
+const server = browserSync.create();
+
+const paths = {
+    source: {
+        pug: "./src/**/*.{pug,html}",
+        scss: "./src/scss/**/*.s[ca]ss",
+        fonts: "./src/fonts/*.{woff,woff2,eot,ttf,otf}",
+        images: "./src/images/*",
+        dotfiles: "./src/.*"
+    },
+    dist: {
+        html: "./dist",
+        css: "./dist/css",
+        fonts: "./dist/fonts",
+        images: "./dist/images",
+    }
+};
+
+function html() {
+    return gulp.src(paths.source.pug)
+        .pipe(pug())
+        .pipe(htmlmin())
+        .pipe(gulp.dest(paths.dist.html));
+}
+
+function css() {
+    const plugins = [
+        autoprefixer(),
+        cssnano()
+    ];
+    return gulp.src(paths.source.scss)
+        .pipe(sass())
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest(paths.dist.css));
+}
+
+/**
+ * Copy fonts to 
+ */
+function fonts() {
+    return gulp.src(paths.source.fonts)
+        .pipe(gulp.dest(paths.dist.fonts));
+}
+
+/**
+ * Copy images to the dist folder.
+ */
+function images() {
+    return gulp.src(paths.source.images)
+        .pipe(gulp.dest(paths.dist.images));
+}
+
+/**
+ * Copy dotfiles. Currently just for .nojekyll, so I can use GitHub pages to
+ * host my personal website.
+ */
+function dotfiles() {
+    return gulp.src(paths.source.dotfiles)
+        .pipe(gulp.dest(paths.dist.html));
+}
+
+function clean() {
+    return del(['dist']);
+}
+
+function reload(done) {
+    server.reload();
+    done();
+}
+
+function serve(done) {
+    server.init({
+        server: {
+            baseDir: './dist'
+        }
+    });
+    gulp.watch(paths.source.pug, gulp.series(html, reload));
+    gulp.watch(paths.source.scss, gulp.series(css, reload));
+    gulp.watch(paths.source.fonts, gulp.series(fonts, reload));
+    gulp.watch(paths.source.images, gulp.series(images, reload));
+    done();
+}
+
+export default gulp.series(clean, fonts, dotfiles, images, css, html, serve);
+export const build = gulp.series(clean, dotfiles, fonts, images, css, html);
+
